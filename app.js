da0623843aa7296fcca4a1ff6393b09f8e15fc17
mahizaman <77525691+mahizaman@users.noreply.github.com>
2021-11-12 23:22:10 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("gallery-item")){
+          const src = e.target.getAttribute("src");
+          document.querySelector(".modal-img").src = src;
+          const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+          myModal.show();
+    }
+  })
+
+
+
+
+  
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+      
+
+let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', function(){
+  spinnerWrapper.style.display= 'none';
+});
